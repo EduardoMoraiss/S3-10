70613c8cb3833a92def7f985b0645345989c6271
Eduardo Morais <103143539+EduardoMoraiss@users.noreply.github.com>
2022-04-18 15:27:00 -0300

    Terceira tentativa

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let competidores = [];
 function positions(firstPlace, secondPlace, lastPlace) {
+  let competidores = [];
   if (lastPlace === "Daniel") {
     competidores.push(
       `1 - Colocado: ${firstPlace} 2 - Colocado: ${lastPlace} 3 - Colocado: ${secondPlace}`
     );
+  } else if (secondPlace === "Daniel") {
+    competidores.push(
+      `1 - Colocado: ${secondPlace} 2 - Colocado: ${firstPlace} 3 - Colocado: ${lastPlace}`
+    );
+  } else {
+    return `1 - Colocado: ${firstPlace} 2 - Colocado: ${secondPlace} 3 - Colocado: ${lastPlace}`;
   }
-  return competidores;
 }
-console.log(positions("Rafael", "Manoel", "Daniel"));
+console.log(positions("Daniel", "Rafael", "Manoel"));
